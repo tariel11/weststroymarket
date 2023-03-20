28fbc8c5d1f8900b3d80a535d57ff1fc0b111958
Tariel Arslanov <tarielarslanov5@gmail.com>
2023-03-21 00:12:13 +0300

    new productpage

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,116 +1,136 @@
-import React, { useEffect, useState } from 'react'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import QuantityButtons from '../components/UI/QuantityButtons'
-import CatalogBar from '../components/catalogBar/CatalogBar'
-import useMediaQuery from '../hooks/useMediaQuery'
-import { useDispatch, useSelector } from 'react-redux'
-import { decreaseProduct, increaseProduct } from '../store/slices/basket'
-import {  useParams } from 'react-router-dom'
-import { addToWishList } from '../store/slices/wishList'
-import axios from '../utils/axios'
+import React, { useEffect, useState } from "react";
+import QuantityButtons from "../components/UI/QuantityButtons";
+import { useDispatch, useSelector } from "react-redux";
+import { decreaseProduct, increaseProduct } from "../store/slices/basket";
+import { useParams } from "react-router-dom";
+import { addToWishList } from "../store/slices/wishList";
+import checkIcon from "./../img/icons/check.svg";
+import axios from "../utils/axios";
 
 const ProductPage = () => {
-  const {basket} = useSelector(state => state.basket)
-  const {basketItems} = basket
+    const { basket } = useSelector((state) => state.basket);
+    const { basketItems } = basket;
 
-  const {title} = useParams()
-  const dispatch = useDispatch()
-  const isDesktop = useMediaQuery("(min-width: 768px)"); 
-  const [isAdded, setIsAdded] = useState(false)
-  const [isLiked, setIsLiked] = useState(false)
-  const [isElem, setIsElem] = useState('product')
-  const [product, setProduct] = useState([])
+    const { title } = useParams();
+    const dispatch = useDispatch();
+    const [isLiked, setIsLiked] = useState(false);
+    const [product, setProduct] = useState([]);
 
-  useEffect(() => {
-    const fetchOneProduct = async  () => {
-      try {
-        const {data} = await axios.get(`/products/card`, {params: {title}})
-        setProduct(data)
-      } catch (err) {
-        console.log(err);
-      }
+  const storeProduct = basketItems.filter(item => item.title === product.title)[0]
+
+    useEffect(() => {
+        const fetchOneProduct = async () => {
+            try {
+                const { data } = await axios.get(`/products/card`, {
+                    params: { title },
+                });
+                setProduct(data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchOneProduct();
+    }, [title]);
+    const handleDecreaseProduct = () => {
+      dispatch(decreaseProduct(product)) 
     }
-    fetchOneProduct()
-  }, []);
-
   
-  const handleIncreaseProduct = () => {
-    dispatch(increaseProduct(product))
-    setIsAdded(true)
-  }
+    const handleIncreaseProduct = (product) => {
+      dispatch(increaseProduct(product))
+    }
+  
+    const handleAddToWishList = () => {
+      dispatch(addToWishList(product))
+      setIsLiked(true)
+    }
 
-  const handleAddToWishList = () => {
-    dispatch(addToWishList(product))
-    setIsLiked(true)
-  }
+    return (
+        <div className="w-5/6 mx-auto pb-10">
+            <h1 className="text-2xl mb-5 sm:text-3xl md:text-4xl font-semibold">
+                {product.category}
+            </h1>
+            <div className="flex flex-col p-5 gap-5 mb-5 md:flex-row">
+                <div className="shadow flex-1">
+                    <img
+                        src={product.img}
+                        alt={product.title}
+                        className="mx-auto"
+                    />
+                </div>
+                <div className="flex-1 flex flex-col justify-end">
+                    <h2 className="text-xl sm:text-2xl md:text-3xl font-medium mb-auto">
+                        {product.title}
+                    </h2>
+                    <div className="flex items-center gap-2 mb-4">
+                        {" "}
+                        <img src={checkIcon} alt="checkIcon" />{" "}
+                        <span className=" text-[#3BAA35]">В наличии</span>
+                    </div>
+                    <i className="mb-3">
+                        Артикул <span>{product.article}</span>{" "}
+                    </i>
+                    <div>
+                        <p className="mb-4 font-semibold">
+                            Цена товара {product.price} ₽
+                        </p>
 
-  return (
-    <section>
-      <div className='w-5/6 mx-auto py-7 sm:grid grid-cols-[2.5fr,7.5fr] gap-5'>
+                        {isLiked ? (
+                            <p className="text-center p-1 bg-green-500 mb-3">
+                                Добавлено в отложенные
+                            </p>
+                        ) : (
+                            <button
+                                onClick={() =>
+                                    !isLiked && handleAddToWishList()
+                                }
+                                className=" w-full mb-3 p-1 bg-slate-300"
+                            >
+                                Добавить в отложенные
+                            </button>
+                        )}
 
-      {isDesktop && <CatalogBar/>}
-
-      <div>
-        <h1 className='text-2xl xs:text-4xl mb-5'>{product.title}</h1>
-
-        <div className='grid grid-cols-[auto,auto,auto,auto] text-xs border-t-2 overflow-hidden text-center'>
-          <p onClick={()=>setIsElem('product')} className={`cursor-pointer border-x-2 border-b-2 bg-slate-100 p-2 ` + (isElem === 'product' ? "border-b-0 bg-white" : "")}>Товар</p>
-          <p onClick={()=>setIsElem('description')} className={`cursor-pointer border-x-2 border-b-2 bg-slate-100 p-2 ` + (isElem === 'description' ? "border-b-0 bg-white" : "")}>Описание</p>
-          <p onClick={()=>setIsElem('info')} className={`cursor-pointer border-x-2 border-b-2 bg-slate-100 p-2 ` + (isElem === 'info' ? "border-b-0 bg-white" : "")}>Характеристики</p>
-          <p onClick={()=>setIsElem('delivery')} className={`cursor-pointer border-x-2 border-b-2 bg-slate-100 p-2 ` + (isElem === 'delivery' ? "border-b-0 bg-white" : "")}>Доставка</p>
+                        {storeProduct ? (
+                            <QuantityButtons
+                                product={product}
+                                handleDecreaseProduct={handleDecreaseProduct}
+                                handleIncreaseProduct={handleIncreaseProduct}
+                                quantity={storeProduct.quantity}
+                            />
+                        ) : (
+                            <button
+                                className=" w-full p-1 bg-yellow-500"
+                                onClick={() => handleIncreaseProduct(product)}
+                            >
+                                В корзину
+                            </button>
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className="p-5 bg-[#F6F6F6] flex flex-col gap-4 rounded-2xl border">
+                <h2 className="text-xl sm:text-2xl md:text-3xl font-medium">
+                    Описание
+                </h2>
+                <p>
+                    Штукатурка Волма Слой, 30кг Штукатурка на гипсовой основе
+                    для ручного нанесения.
+                </p>
+                <p>
+                    НАЗНАЧЕНИЕ: <br /> Выравнивание оснований в помещениях
+                    нормальной влажности перед нанесением декоративного слоя;
+                    Создание ровной поверхности на цементных,
+                    цементно-известковых, бетонных, кирпичных, гипсовых и
+                    гипсокартонных плоскостях.
+                </p>
+                <p>
+                    ПРЕИМУЩЕСТВА: <br /> Экономичная (расход продукта 8-9 кг на
+                    1 кв.м, не требует покрытия шпаклевкой); Пластичная; Слой
+                    нанесения от 5 до 60мм; Можно использовать для создания
+                    декоративных поверхностей.
+                </p>
+            </div>
         </div>
+    );
+};
 
-        {isElem === 'product' && 
-        <div className='ss:grid grid-cols-2 md:grid-cols-[2fr,1fr] gap-5 items-center'>
-          <div>
-            <img className='block mx-auto' src={product.img} alt="alt" />
-          </div>
-        
-          <div className="buy">
-            <p className='mb-4 font-semibold'>Цена товара {product.price} P</p>
-            <button className=' w-full mb-3 p-1 bg-slate-300'>Купить в клик</button>
-
-            {isLiked 
-            ?  <p className='text-center p-1 bg-green-500 mb-3'>Добавлено в отложенные</p>
-            : <button onClick={() => !isLiked && handleAddToWishList()}className=' w-full mb-3 p-1 bg-slate-300'>Добавить в отложенные <FontAwesomeIcon icon={faHeart} className={`cursor-pointer ` + (isLiked ? ' text-red-500' : ' text-slate-400') }/></button> }
-            
-           {isAdded 
-           ? <p className='text-center p-1 bg-green-500'>Добавлено в корзину</p>
-           : <button className=' w-full p-1 bg-yellow-500' onClick={handleIncreaseProduct}>В корзину</button> }
-              
-          </div>
-        </div>}
-
-        {isElem === 'description' && 
-        <div className='py-5'>
-          <h3 className='text-xl mb-4'>Описание</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deserunt, maiores et perspiciatis ipsa sit sapiente dolorem cupiditate, itaque ea facere veritatis dignissimos, aliquam perferendis quaerat illum explicabo ab ullam.</p>
-        </div>}
-
-        {isElem === 'info' && 
-        <div className='py-5'>
-          <h3 className='text-xl mb-4'>Характеристики</h3>
-          <ul>
-            <li>Бренд................Старатели</li>
-            <li>Бренд................Старатели</li>
-            <li>Бренд................Старатели</li>
-            <li>Бренд................Старатели</li>
-            <li>Бренд................Старатели</li>
-          </ul>
-        </div>}
-
-        {isElem === 'delivery' && 
-        <div className='py-5'>
-          <h3 className='text-xl mb-4'>Доставка</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deserunt, maiores et perspiciatis ipsa sit sapiente dolorem cupiditate, itaque ea facere veritatis dignissimos, aliquam perferendis quaerat illum explicabo ab ullam.</p>
-        </div>}
-
-      </div>
-
-     </div>
-    </section>
-  )
-}
-
-export default ProductPage
+export default ProductPage;
